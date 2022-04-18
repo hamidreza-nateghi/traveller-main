92f9448381e6aa395c8b5178686191c54fd772e0
Hamidreza <hamidrezatemp@gmail.com>
2022-04-18 23:55:05 +0430

    add apollo provider to test utils

--- packages/client/src/test-utils.tsx
+++ packages/client/src/test-utils.tsx
@@ -2,9 +2,16 @@ import * as React from 'react'
 import type { RenderOptions } from '@testing-library/react'
 import { render } from '@testing-library/react'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+})
 
 const AllProviders = ({ children }: { children?: React.ReactNode }) => (
-  <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  <ChakraProvider theme={theme}>
+    <ApolloProvider client={client}>{children}</ApolloProvider>
+  </ChakraProvider>
 )
 
 const customRender = (ui: React.ReactElement, options?: RenderOptions) =>
