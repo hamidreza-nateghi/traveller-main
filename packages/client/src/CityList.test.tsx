491352abae11b4e6b8bede1e29dbae15afbf6e91
Hamidreza <hamidrezatemp@gmail.com>
2022-04-18 23:56:59 +0430

    add city list tests

---
+++ packages/client/src/CityList.test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react'
+import { render } from './test-utils'
+import { CityList } from './CityList'
+
+describe('<CityList /> component', () => {
+  it('renders no data when list is empty', () => {
+    const { getByText } = render(<CityList list={[]} />)
+    expect(getByText(/^No city found.$/i)).toBeTruthy()
+  })
+
+  it('renders data when list is not empty', () => {
+    render(<CityList list={[{ id: 0, name: 'London', country: 'United Kingdom', visited: false, wishlist: false }]} />)
+    expect(screen.queryByText(/^No city found.$/i)).not.toBeInTheDocument()
+  })
+})
