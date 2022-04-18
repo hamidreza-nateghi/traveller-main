bffe7118235a3694353c6d145f841b21052f380c
Hamidreza <hamidrezatemp@gmail.com>
2022-04-18 23:56:50 +0430

    add city card tests

---
+++ packages/client/src/CityCard.test.tsx
@@ -0,0 +1,17 @@
+import { screen } from '@testing-library/react'
+import { render } from './test-utils'
+import { CityCard } from './CityCard'
+
+describe('<CityCard /> component', () => {
+  beforeEach(() => {
+    render(<CityCard id={0} name="London" country="United Kingdom" visited={false} wishlist={false} />)
+  })
+
+  it('renders city name', () => {
+    expect(screen.getByText(/^London$/i)).toBeTruthy()
+  })
+
+  it('renders country name', () => {
+    expect(screen.getByText(/^United Kingdom$/i)).toBeTruthy()
+  })
+})
